d09c5f6ecb9d9230f34bbc62be6b68f359e649a5
Christopher Soncrant <christopher.soncrant@gmail.com>
2022-12-03 11:01:35 -0800

    added the logic for the item tags

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -25,18 +25,44 @@ const ShoeCard = ({
   // both on-sale and new-release, but in this case, `on-sale`
   // will triumph and be the variant used.
   // prettier-ignore
-  const variant = typeof salePrice === 'number'
+  const variant = typeof salePrice === 'number' 
     ? 'on-sale'
     : isNewShoe(releaseDate)
       ? 'new-release'
       : 'default'
+  ;
 
+  const TAGSTYLES = {
+    onSale: {
+      bgColor: '#C5295D',
+      display: 'block',
+      text: 'On Sale!',
+    },
+    newRelease: {
+      bgColor: '#6868D9',
+      display: 'block',
+      text: 'Just Released!'
+    },
+    default: {
+      bgColor: '#6868D9',
+      display: 'none',
+    }
+  }
+
+  const tagDisplay = variant === "default" 
+    ? TAGSTYLES.default 
+    : variant === "on-sale" 
+    ? TAGSTYLES.onSale
+    : TAGSTYLES.newRelease
+  ;
+
+  console.log(tagDisplay)
   return (
     <Link href={`/shoe/${slug}`}>
       <Wrapper>
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
-          <Tag bcolor={"yellow"}>{variant}</Tag>
+          <Tag show={tagDisplay}>{tagDisplay.text}</Tag>
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
@@ -65,12 +91,14 @@ const ImageWrapper = styled.div`
 `;
 
 const Tag = styled.div`
+  display: ${props => props.show.display};
   position: absolute;
   top: 12px;
   right: -5px;
   padding: 4px 16px;
-  background-color: green;
-  color: ${prop => prop.bcolor};
+  background-color: ${props => props.show.bgColor};
+  color: ${COLORS.white};
+  border-radius: 3px;
 `;
 
 const Image = styled.img`
